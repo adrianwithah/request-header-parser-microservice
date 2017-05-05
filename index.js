93c4e8cdce0a16659507c2588d8b636c787224c9
boyinstruggle <lim.weiheng.hadrian@gmail.com>
2017-05-05 12:19:04 +0800

    Added homepage functionality

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require("express");
 var app = express();
 var useragent = require("useragent");
+var path = require("path");
+
+app.set("views",path.join(__dirname,"views","pages"));
+app.set("view engine","ejs");
+
+app.get("/",function(request,response) {
+	response.render("homepage");
+});
 
 app.get("/api/whoami",function(request, response) {
 	var ipaddress = request.connection.remoteAddress;
